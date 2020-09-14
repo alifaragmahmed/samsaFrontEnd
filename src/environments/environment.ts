0bb70a1fddd9882bfb06adf7626e9024485deb3b
programmerAly <mahmedosama2016@gmail.com>
2020-09-14 10:32:31 +0200

    change account module

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiUrl      : 'http://41.41.86.210:4000/admin/api',
   // backEndPublicUrl   : 'http://41.41.86.210:4000/',
-  apiUrl: 'http://127.0.0.1:8000/api',
+  apiUrl: 'http://localhost/samsa/public/api',
   backEndPublicUrl: 'http://localhost:4000/',
   googleApiKey: 'AIzaSyAtCTUfmq6JOFrE9Ib_HPEPFhUE9VdwcEs',
   frontEndPublicUrl: 'http://127.0.0.1:4200/'
