2e1fdcefb92d825af204522522650dd05de45979
Mustafa Elgammal <gogo202046@yahoo.com>
2020-09-21 20:26:09 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,13 +22,12 @@ const routes: Routes = [
         loadChildren: './settings/settings.module#SettingsModule'
       },
       {
-<<<<<<< HEAD
         path: 'adminision',
         loadChildren: './adminision/adminision.module#AdminisionModule'
-=======
+      },
+       {
         path: 'military',
         loadChildren: './military/military.module#MilitaryModule'
->>>>>>> 7630432df40f8df6f4098541fcbb8bfaacd3f793
       },
      /* {
         path: 'dashboard',
